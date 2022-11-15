8866248a394db9572928ace4d54fb226ad0d120d
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-15 16:57:35 -0300

    feat: add the globalstyle component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LogInPage from "./pages/LogInPage/LogInPage";
+import GloboStyle from "./style/GloboStyle";
 
 function App() {
   return (
     <div className="App">
+    <GloboStyle/>
     <LogInPage/>
     </div>
   );
