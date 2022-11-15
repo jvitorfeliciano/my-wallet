8d49bda71e0d328ce22439e22ffde10cb72f3bd0
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-15 17:12:02 -0300

    feat: add the Input component and the Raleway font import from google

---
+++ src/components/Input/Input.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  height: 58px;
+  width: 100%;
+  background: #ffffff;
+  border-radius: 5px;
+  padding: 15px;
+  color: #000000;
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  margin-bottom: 13px;
+`;
+
+export default Input;
