00ebe62c37086482a58810f7f34585a7b82c4ee8
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-15 20:01:50 -0300

    feat: add the header to the Extract Page

--- src/pages/ExtractPage/ExtractPage.js
+++ src/pages/ExtractPage/ExtractPage.js
@@ -1,9 +1,44 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
-export default function ExtractPage(){
-
-    return(
-        <div>oieee</div>
-    )
+export default function ExtractPage() {
+  return (
+    <Container>
+      <Header>
+        <UserName>Olá, Fulano</UserName>
+        <RiLogoutBoxRLine />
+      </Header>
+    </Container>
+  );
 }
+
+const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #8c16be;
+  padding: 21px;
+`;
+const Header = styled.section`
+  width: 100%;
+  background: red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  svg {
+    color: #ffffff;
+    font-size: 30px;
+  }
+`;
+const UserName = styled.span`
+ 
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    color: #FFFFFF;
+  
+`
