97039f66746a2d0e17b113f5bf32e39bb925d050
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-15 22:43:10 -0300

    doc: add the services folder to interact with the api

---
+++ src/services/api.js
@@ -0,0 +1,38 @@
+const BASE_URL = "http://localhost:5000";
+
+function createConfig(token) {
+  return { headers: { Authorization: `Bearer ${token}` } };
+}
+
+function signUp(body) {
+  const promise = axios.post(`${BASE_URL}/sign-up`, body);
+
+  return promise;
+}
+
+function logIn(body) {
+  const promise = axios.promise(`${BASE_URL}/sign-in`, body);
+  return promise;
+}
+
+function getExtract(token) {
+  const config = createConfig(token);
+  const promise = axios.get(`${BASE_URL}/extract`, config);
+  return promise;
+}
+
+function postExtract(body, token) {
+  const config = createConfig(token);
+  const promise = axios.post(`${BASE_URL}/extract`, body, config);
+  return promise;
+}
+
+const api = {
+  signUp,
+  logIn,
+  getExtract,
+  postExtract,
+};
+
+export default api;
+
